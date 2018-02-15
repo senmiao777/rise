0ed8bdcd2f83d3b878b6375284058791585ebddf
13240411778 <13240411778@163.com>
2018-02-15 16:33:18 +0800

    字符串api

--- com.frank/com.frank.test.py/StringTest.py
+++ com.frank/com.frank.test.py/StringTest.py
@@ -17,3 +17,12 @@ print("split 2 result =", split)
 # 按正则表达式替换
 sub = re.sub('[r]', '1234', str)
 print(sub)
+
+
+def read_txt(path):
+    with open(path, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+    return lines
+
+
+print('读取txt result=', read_txt('G:\pycharm\pytest.txt'));
