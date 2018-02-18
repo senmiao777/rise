c19423922d9f6c143342f41ff0cb8d5474d12d57
13240411778 <13240411778@163.com>
2018-02-18 13:09:57 +0800

    basicgrammar

---
+++ com.frank/com.frank.test.py/basicgrammar.py
@@ -0,0 +1,8 @@
+# print('Hello World')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('Hello World')
+name = input("please input your name: ")
+print("hello: ", name)
