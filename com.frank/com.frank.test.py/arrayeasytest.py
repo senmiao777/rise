b369ae48b4ad6c8f7ff983feea76cb62f39bcbb9
13240411778 <13240411778@163.com>
2018-03-05 22:38:10 +0800

    raise 抛出异常

--- com.frank/com.frank.test.py/arrayeasytest.py
+++ com.frank/com.frank.test.py/arrayeasytest.py
@@ -13,11 +13,26 @@ def twoSum(numbers, target):
             # result.append(map[str(b)])
             # result.append(i)
             # return result
-            return [map[str(b)],i]
+            return [map[str(b)], i]
         else:
             map[str(numbers[i])] = i
+
+
+def remove_duplicate(numbers):
+    n = 0;
+    for i in range(1, len(numbers)):
+        # 从第0个开始比
+        if (numbers[n] != numbers[i]):
+            n = n + 1
+            # 从第一个开始记
+            numbers[n] = numbers[i]
+    return n + 1
 
 
 num = [1, 4, 11, 15, 17]
 target = 26
 print("twoSum=", twoSum(num, target))
+
+num = [1, 1, 1, 2, 3, 3, 3, 4, 4, 5, 6, 7]
+print("remove_duplicate=", remove_duplicate(num))
+print("remove_duplicate number =", num)
