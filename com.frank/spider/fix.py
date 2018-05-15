8ad7eabe95f15069fcd59d72133ff9ceb1a7546b
guojingfeng <jingfeng.guo@quantgroup.cn>
2018-05-15 17:58:14 +0800

    读取txt,循环调用接口

---
+++ com.frank/spider/fix.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import requests
+import json
+import pymysql
+import re
+import time
+
+def send(_age, _name, _type):
+    head = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+    }
+    ##post发送的数据
+    postData = {
+        'name': _name,
+        'age': _age,
+        'type': _type,
+        'key': 'BO123'
+    }
+
+## 必须是http（s）://开头
+    _url="http://www.baidu.com"
+    response = requests.post(url=_url, headers=head, data=postData)
+    json_result = json.loads(response.text)
+    print("response code =", json_result.get('code'))
+    return json_result.get('code')
+
+
+def read_txt():
+    with open(file="E:/test2.txt", mode="r", encoding="utf-8") as f:
+        # 为a+模式时，因为为追加模式，指针已经移到文尾，读出来的是一个空字符串。
+        #   ftext = f.read()  # 一次性读全部成一个字符串
+        ftextlist = f.readlines()  # 也是一次性读全部，但每一行作为一个子句存入一个列表
+        lines = len(ftextlist)
+        print("lines =", lines)
+        for i in range(lines):
+            split = ftextlist[i].split(",")
+            id = split[0]
+            term = split[1]
+            print("split[0] = ", id)
+            print("split[1] = ", term)
+            send_result = send(id.strip(), term.strip(), 'all')
+            time.sleep(0.1)
+    return 0
+
+
+# print("result = ",send(1,1,1))
+
+print("result = ", read_txt())
