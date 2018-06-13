e231bc300dbb54d3f733b7349c1593fedb90d81e
13240411778 <13240411778@163.com>
2018-06-14 00:08:14 +0800

    定时任务

--- com.frank/learn/ej.py
+++ com.frank/learn/ej.py
@@ -5,11 +5,17 @@ print("Hello World")
 
 def job():
     print("job doing ...")
+    with open(file="E:/t1.txt", mode="r+", encoding="utf-8") as f:
+        ftextlist = f.readlines()
+        print("content=",ftextlist)
+        t= time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+        f.write("job doing"+t+"\n")
+    print("job finish")
 
-name = 'zhangsan'
-print(job())
+print("start running...")
 
 schedule.every(5).seconds.do(job)
 while True:
     schedule.run_pending()
     time.sleep(1)
+
